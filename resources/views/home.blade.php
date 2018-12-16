66daa209933e8ac020840b1c85740fbe6628e648
sagarshah4819 <ss3958@njit.edu>
2018-12-15 23:45:12 -0500

    Task: Associated the questions with the user-id.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,6 +28,7 @@
                                                     <a class="btn btn-primary float-right" href="{{ route('question.show', ['id' => $question->id]) }}">
                                                         View
                                                     </a>
+                                                <p class="card-text">{{$question->user_id}}</p>
                                                 </p>
                                             </div>
                                         </div>
